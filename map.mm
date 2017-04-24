77e2de4a9ddd1c6fe6b549ffc01869e15871f7a5
Gauthiervangre <vangrevelynghe.gauthier@hotmail.com>
2017-04-24 12:25:02 +0200

    Add files via upload

---
+++ map.mm
@@ -0,0 +1,99 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="index" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1490942558451"><hook NAME="MapStyle" zoom="0.751">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<edge COLOR="#999999" WIDTH="2"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="0"/>
+<node TEXT="Affichage des ventes" POSITION="right" ID="ID_1797864921" CREATED="1490266737072" MODIFIED="1490943241130" HGAP="120" VSHIFT="-100">
+<edge COLOR="#0000ff"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="240" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_767634615" STARTINCLINATION="277;0;" ENDINCLINATION="277;-38;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+<node TEXT="Affichage des locations" POSITION="right" ID="ID_36400205" CREATED="1490266779982" MODIFIED="1490941548120" HGAP="170" VSHIFT="-20">
+<edge COLOR="#00ffff"/>
+<node TEXT="Fiche d&#xe9;tail du bien" ID="ID_767634615" CREATED="1490943214467" MODIFIED="1490943226369" HGAP="66" VSHIFT="-14"/>
+</node>
+<node TEXT="Connexion" POSITION="left" ID="ID_1632083251" CREATED="1490266787229" MODIFIED="1490942167124" HGAP="180" VSHIFT="110">
+<edge COLOR="#ffff00"/>
+<hook NAME="FirstGroupNode"/>
+<node TEXT="Index (Administrateur)" ID="ID_249140380" CREATED="1490941583654" MODIFIED="1490942686468" VSHIFT="-100">
+<edge COLOR="#ff6600"/>
+<node TEXT="Ajouter un bien (ajax)" ID="ID_1502476802" CREATED="1490942733381" MODIFIED="1490942767729" HGAP="56" VSHIFT="-153"/>
+<node TEXT="Modifier (ajax)/supprimer un bien" ID="ID_638058780" CREATED="1490942770887" MODIFIED="1490942847882" HGAP="137" VSHIFT="-91"/>
+<node TEXT="Affichage des ventes" ID="ID_535161216" CREATED="1490942868324" MODIFIED="1490943258992" HGAP="264" VSHIFT="-19">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="180" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_77616709" STARTINCLINATION="383;0;" ENDINCLINATION="383;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+<node TEXT="Affichage des locations" ID="ID_1980384612" CREATED="1490942948037" MODIFIED="1490943256262" HGAP="333" VSHIFT="24">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="180" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_77616709" STARTINCLINATION="284;0;" ENDINCLINATION="284;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+</node>
+<node TEXT="Index (Membre)" ID="ID_1638435828" CREATED="1490941587000" MODIFIED="1490942717291" HGAP="50" VSHIFT="70">
+<edge COLOR="#ff6633"/>
+<node TEXT="Ajouter un bien (vente ou location)" ID="ID_654607893" CREATED="1490941743676" MODIFIED="1490941765034" HGAP="110" VSHIFT="-10"/>
+<node TEXT="Voir ses biens" ID="ID_1085371111" CREATED="1490941767117" MODIFIED="1490942098502" HGAP="200" VSHIFT="20">
+<node TEXT="Fiche d&#xe9;tail du bien" ID="ID_77616709" CREATED="1490942912229" MODIFIED="1490942921058" HGAP="183" VSHIFT="12"/>
+</node>
+<node TEXT="Modifier/supprimer ses biens" ID="ID_535963512" CREATED="1490941783693" MODIFIED="1490941795846" HGAP="150" VSHIFT="40"/>
+<node TEXT="Voir ses informations personnelles" ID="ID_1235251572" CREATED="1490941871815" MODIFIED="1490941890147" HGAP="100" VSHIFT="50">
+<node TEXT="Modifer ses informations personnelles (ajax)" ID="ID_505415469" CREATED="1490941894179" MODIFIED="1490943283023" HGAP="120"/>
+</node>
+<node TEXT="Affichage des ventes" ID="ID_299205542" CREATED="1490941972097" MODIFIED="1490943252580" HGAP="170" VSHIFT="110">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="160" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_77616709" STARTINCLINATION="405;0;" ENDINCLINATION="405;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+<node TEXT="Affichage des locations" ID="ID_745626103" CREATED="1490941990587" MODIFIED="1490943247027" HGAP="160" VSHIFT="100">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="220" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_77616709" STARTINCLINATION="499;0;" ENDINCLINATION="499;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+</node>
+</node>
+<node TEXT="Inscription" POSITION="left" ID="ID_1322651848" CREATED="1490941567443" MODIFIED="1490943300791" HGAP="83" VSHIFT="-387">
+<edge COLOR="#00cc33"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="180" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1638435828" STARTINCLINATION="-344;80;" ENDINCLINATION="165;145;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+</node>
+</map>
